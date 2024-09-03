1ae34e70c2264dd5b4c36e8d8a3c44f056ae60dd
gojekarthik <karthikgoje99@gmail.com>
2024-09-03 10:25:01 +0530

    changes to auth.tsx

--- frontend/src/components/auth.tsx
+++ frontend/src/components/auth.tsx
@@ -109,7 +109,6 @@ function LableInput({ lable, placeholder, type, onchange }: LabbledInputs) {
       <input
         onChange={onchange}
         type={type}
-        id="first_name"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 mb-3 "
         placeholder={placeholder}
         required
